aafcef514f3b9177a6cfdb64650bf0b73c3fa891
Ann Yun <by.ann.yun@gmail.com>
2024-10-27 12:04:33 -0400

    completed write and echo functionality

--- ldd.c
+++ ldd.c
@@ -1,11 +1,20 @@
+/*
+ * A linux driver that creates a file in /proc at initialization,
+ * and allow the user to write to it and read back from it.
+ */
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 
+#define MAX_BUFFER_SIZE 1024
+
+/* some module info */
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Ann Yun");
 MODULE_DESCRIPTION("A toy loadable kernel driver");
 
+/* variables and functions */
+static char driver_buffer[MAX_BUFFER_SIZE];
 static struct proc_dir_entry *my_file_node;
 static ssize_t read_op(struct file *f, char __user *user_buffer, size_t size, loff_t *offset);
 static ssize_t write_op(struct file *f, const char __user *user_buffer, size_t size, loff_t *offset);
@@ -15,14 +24,13 @@ struct proc_ops fn_proc_ops = {
 };
 
 static ssize_t read_op(struct file *f, char __user *user_buffer, size_t size, loff_t *offset){
-	char msg_to_ubuffer[] = "read\n";
-	size_t str_len = sizeof(msg_to_ubuffer);	// this includes the null char
+	size_t str_len = sizeof(driver_buffer);	// this includes the null char
 	unsigned long nbytes_not_copied;
 
 	pr_debug("read_op: entry\n");
 	pr_debug("read_op: str_len: %lu, size: %lu, offset: %llu\n", str_len, size, *offset);
 
-	nbytes_not_copied = copy_to_user(user_buffer, msg_to_ubuffer, str_len);
+	nbytes_not_copied = copy_to_user(user_buffer, driver_buffer, str_len);
 	if (*offset >= str_len || nbytes_not_copied != 0)	return 0;
 	*offset += str_len;
 
@@ -32,20 +40,28 @@ static ssize_t read_op(struct file *f, char __user *user_buffer, size_t size, lo
 }
 
 static ssize_t write_op(struct file *f, const char __user *user_buffer, size_t size, loff_t *offset){
-	size_t str_len = size;
-	char kernel_buffer[size];
+	/*
+	 * Get a user input and write it into the buffer.
+	 * If the input is too long for the buffer, raise an error.
+	 */ 
 	unsigned long nbytes_not_copied;
 
 	pr_debug("write_op: entry\n");
-	pr_debug("write_op: str_len: %lu, size: %lu, offset: %llu size_max: %lu\n", str_len, size, *offset, SIZE_MAX);
+	pr_debug("write_op: size: %lu, offset: %llu\n", size, *offset);
 
-	nbytes_not_copied = copy_from_user(kernel_buffer, user_buffer, str_len);
-	if (*offset >= str_len || nbytes_not_copied != 0)	return 0;
-	*offset += str_len;
+	// size includes the null char at the end of user_buffer's string
+	if (size > MAX_BUFFER_SIZE){
+		pr_warn("Input longer than max buffer size is not allowed");
+		return -ENOMEM;
+	}
+
+	nbytes_not_copied = copy_from_user(driver_buffer, user_buffer, size);
+	if (*offset >= size || nbytes_not_copied != 0)	return 0;
+	*offset += size;
 
 	pr_debug("write_op: exit\n");
 
-	return str_len;
+	return size;
 }
 
 static int my_module_init(void){
