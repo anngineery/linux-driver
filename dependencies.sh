1e7a701b88a1282880515fc7bae8ded7d92e1d3f
Ann Yun <by.ann.yun@gmail.com>
2024-10-29 17:00:19 -0400

    More sophisticated. Add python dependencies too

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade
-sudo apt install -y build-essential linux-headers-$(uname -r) kmod
+declare -a PKG_NAMES=(
+	"linux-headers-$(uname -r)"
+	"kmod"
+	"build-essential"
+	)
+
+sudo apt update &>/dev/null
+sudo apt -y upgrade &>/dev/null
+
+for i in "${PKG_NAMES[@]}"
+do
+	echo checking for "$i"
+	if [[ -z "$(dpkg -s "$i" | grep -o 'install ok installed')" ]]; then
+		echo Installing: "$i"
+		sudo apt install "$i"
+	fi
+done
+
+echo checking for pip
+if [[ -z "$(command -v pip)" ]]; then
+	echo Installing: pip 
+	sudo apt install python3-pip
+fi
+
+echo checking for pytest
+if [[ -z "$(pip list | grep pytest)" ]]; then
+	echo Installing: pytest
+	sudo apt install python3-pytest
+fi
